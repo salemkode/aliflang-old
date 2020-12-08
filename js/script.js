224fc9c8c2e8f481e886b67e7ff4bcacd5306236
superastorh <salem.m.s2030@gmail.com>
2020-12-08 14:57:30 +0300

    update 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 //hermes
 let req = new XMLHttpRequest();
 let names = null;
+
+
 req.open("GET", "data/json.json");
 req.onload = () => {
   names = JSON.parse(req.responseText);
@@ -13,17 +15,18 @@ req.send();
 var OSbu = document.querySelector("#os");
 var OSName = "ويندوز";
 var OSicon = "windows";
-var OSlink = "Alif_Studio.msi"; 
+var OSlink = "Alif_Studio.msi";
 
 if (navigator.appVersion.indexOf("Mac") != -1) {
   OSName = "ماك";
-  OSicon = "apple"
-  OSlink = "Alif_Studio_macOS.zip"
+  OSicon = "apple";
+  OSlink = "Alif_Studio_macOS.zip";
 } else if (navigator.appVersion.indexOf("Linux") != -1) {
   OSName = "لينكس";
   OSicon = "linux";
-  OSlink = "AlifStudio2.tar.gz"
+  OSlink = "AlifStudio2.tar.gz";
 }
 
 OSbu.innerHTML = `<a href="https://www.aliflang.org/alifstudio/${OSlink}" target="_blank">
 <i class="fab fa-${OSicon}"></i> تحميل لنضام <span id="os"> ${OSName} </span></a>`;
+
